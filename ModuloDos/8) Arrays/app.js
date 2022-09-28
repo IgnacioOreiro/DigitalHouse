aaec99b7e09c3123f1db5b8b4d9431faf8f0f298
ignacioOreiro <ignaciooreiro3@outlook.es>
2022-09-28 15:08:15 -0300

    Agregue Arrays

---
+++ ModuloDos/8) Arrays/app.js
@@ -0,0 +1,17 @@
+// Los Arrays nos permiten generar una coleccion de datos ordenados
+
+// Los arrays pueden contener todos los tipos de datos incluso arrays
+
+let miArrayNum = [1,2,3];
+
+let miArrayStr = ["Uno","Dos","Tres"];
+
+let miArrayBoo = [true,false,true];
+
+let miArrayCombo = [1,"dos",true,miArrayNum]
+
+
+// Los arrays empiezan desde el 0 
+
+console.log(miArrayNum[0])
+// RESPUESTA: 1
