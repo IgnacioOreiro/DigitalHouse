f06d70e3f7386de8fb334af7984c34cf04bf337d
ignacioOreiro <ignaciooreiro3@outlook.es>
2022-09-29 11:59:13 -0300

    Agregue JSON

---
+++ ModuloDos/11) JSON/app.js
@@ -0,0 +1,21 @@
+//JSON JAVASCRIPT OBJECT NOTATION
+// Es un formato de texto sencillo utilizado para el intercambio de datos entre distintos sistemas
+
+// JSON Al ser una cadena de texto simple es un formato ideal para transmitir informacion entre sitios y aplicaciones especialmente en cuenta que javaScript esta presente en todos los navegadores modernos 
+
+//JSON.parse()
+
+// Convierte un texto con formato JSON al tipo de dato equivalente de JavaScript
+
+// Recibe una cadena de texto con formato JSON
+
+// Devuelve el mismo dato que recibio en formato JavaScript
+
+
+//JSON.stringify()
+
+// Convertite un tipo de dato de JavaScript en texto en formato JSON
+
+// Recibe una cadena de texto con formato JavaScrip
+
+// Devuelve una cadena de texto con formato JSON
