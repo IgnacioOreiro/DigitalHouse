2cfc655dfda681a9110646ba8b4dafe137497c07
ignacioOreiro <ignaciooreiro3@outlook.es>
2022-09-29 11:16:05 -0300

    Agregue Metodos de Arrays

---
+++ ModuloDos/9) Metodos de arrays/app.js
@@ -0,0 +1,37 @@
+//-----METODOS-----//
+
+// (Nombre del Array).(metodo)(parametro)
+
+
+//.push() -- Ingresa datos al final del Array
+
+miArray.push()
+
+//.pop() -- Saca el ultimo elemento del Array y lo retorna
+
+miArray.pop()
+
+//.shift() -- Eliminia el primer elemento de un Array, retorna el elemento
+
+miArray.shift()
+
+//.unshift() -- agrega uno o varios elementos al principio del Array, retorna la longitud del nuevo Array
+
+miArray.unshift()
+
+//.join() -- Crea un string con todos los elementos del Array, separado por lo que nosotros especifiquemos
+
+miArray.join('*')
+
+//.indexOf() -- Busca el Array que recibe como parametro si no lo encuentra devuelve -1
+
+miArray.indexOf()
+
+//.lastIndexOf() -- Busca el Array al final en vez del principio
+
+miArray.lastIndexOf()
+
+//.includes() -- Busca lo que ingresemos como parametro devuelve un Booleano
+
+miArray.includes()
+
