d33817d109ac1bd25b88cbca02b10b4846415e12
ignacioOreiro <ignaciooreiro3@outlook.es>
2022-10-05 18:21:16 -0300

    Calculadora

---
+++ ModuloDos/10) Haciendo la calculadora/calc/app.js
@@ -0,0 +1,17 @@
+const suma = require('./operaciones/suma');
+const resta = require('./operaciones/resta');
+const multi = require('./operaciones/multi');
+const divi = require('./operaciones/divi');
+
+console.log(suma(2,2));
+console.log(resta(2,2));
+console.log(multi(0,20));
+console.log(divi(200,2));
+
+
+
+
+
+
+
+
---
+++ ModuloDos/10) Haciendo la calculadora/calc/operaciones/multi.js
@@ -0,0 +1,5 @@
+function multi(numA,numB){
+    return numA * numB;
+}
+
+module.exports = multi;
